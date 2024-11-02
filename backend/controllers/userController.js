871b8a2e8efb92904752d7a1d2c31258ded17d69
apedensilva <164123381+apedensilva@users.noreply.github.com>
2024-11-02 14:37:07 +0800

    controller folder added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,117 @@
+const express = require('express')
+const {db} = require('../dbconnection/databaseconnection');
+
+
+//POST USER CONTROLLER
+const createUser = async (req,res) => {
+
+    console.log('Results: ',req.body);
+
+    const pdata = {
+        name: req.body.name,
+        age: req.body.age,
+        address: req.body.address
+    };
+
+    const sql = 'Insert INTO sampledata (name,age,address) VALUES (?,?,?)';
+    const values = [pdata.name,pdata.age,pdata.address]
+
+    db.query(sql,values,(err,results)=>{
+        if(err){
+            console.error('Error inserting data',err);
+            return res.status(500).send({
+                Status_code:500,
+                Message:"Error Inserting data",
+                Error: err.message
+            });
+        }
+        const responseData = {idsampledata: results.insertId,...pdata};
+        console.log("Final Result: ",responseData);
+
+        res.status(200).send({
+            Status_code:200,
+            Message:"User Data Added",
+            User:responseData
+        });
+    });
+
+};
+
+//GET ALL USER CONTROLLER
+const getAllUsers = async (req,res)=>{
+    
+    const sql = 'SELECT * FROM sampledata';
+
+    db.query(sql,(err,results)=>{
+        if(err){
+            console.error('Error fetching data ',err)
+            return res.status(500).send({
+                Status_code:500,
+                Message: "Error fetching data ",
+                Error: err.message
+            })
+        }
+        res.status(200).send(results);
+    })
+
+}
+
+//UPDATE USER
+const updateUser = async (req,res)=>{
+    
+    const userID = req.params.id;
+    const {name,age,address} = req.body;
+
+    const sql = 'UPDATE sampledata SET name = ?, age = ?, address = ? WHERE idsampledata = ?'
+    const values = [name,age,address,userID]
+
+    db.query(sql,values,(err,results)=>{
+        if(err){
+            console.error('Error updating data ',err);
+            return res.status(500).send({
+                Status_code:500,
+                Message:"Error updating data",
+                Error: err.message
+            });
+        }
+        else if(results.affectedRows === 0){
+            return res.status(404).send({
+                Status_code:404,
+                Message:"User Not Found"
+            });
+        }
+        res.status(200).send({
+            Status_code:200,
+            Message:"User Data Updated"
+        })
+    })
+}
+
+//DELETE USER
+
+const deleteUser = async (req,res)=>{
+    const userID = req.params.id
+    const sql = "DELETE FROM sampledata WHERE idsampledata = ?"
+    const values = userID
+    db.query(sql,values,(err,results)=>{
+        if(err){
+            console.error("Failed to Delete selected row ",err)
+            return res.status(500).send({
+                Status_code:500,
+                Message:"Error updating data",
+                Error: err.message
+            });
+        }
+        res.status(200).send({
+            Status_code:200,
+            Message:"User Data Deleted Successfully"
+        })
+    })
+}
+
+module.exports = {
+    createUser,
+    getAllUsers,
+    updateUser,
+    deleteUser
+}
